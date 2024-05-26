7fff25f02fe07ba4a7c938c695469d1501550407
Eddie <>
2024-05-26 19:26:24 +0100

    better packer bootstrapping

--- lua/mainconf/init.lua
+++ lua/mainconf/init.lua
@@ -1,3 +1,24 @@
+-- bootstrap packer
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+	execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+end
+
+execute 'packadd packer.nvim'
+
+require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+
+	if packer_bootstrap then
+		require('packer').sync()
+	end
+end)
+
+
 -- load unrealhero config
 local configFilePath = vim.fn.expand("~") .. "/unrealhero_init.lua";
 local file = io.open(configFilePath, "r")
@@ -16,9 +37,9 @@ vim.g.UltiSnipsJumpForwardTrigger = "<c-b>"
 vim.g.UltiSnipsJumpBackwardTrigger = "<S-Tab>"
 vim.g.UltiSnipsEditSplit = "vertical"
 -- end globals
-require("mainconf.remap")
 require("mainconf.packer")
 require("mainconf.packer_getcommit")
+require("mainconf.remap")
 require("mainconf.set")
 require("mainconf.scrollfontsize")
 
